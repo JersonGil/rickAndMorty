7c8573b71a215994e441494bf7c1b2572e7179c9
Jerson <jerson@mercury.cash>
2021-05-28 07:46:58 -0400

    styles

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react'
 import { gql, useQuery } from '@apollo/client';
-import Location from './Skeleton/Location'
+import Skeleton from './Skeleton/skeleton'
 import Pagination from "react-js-pagination";
+import { Link } from 'react-router-dom'
+import Dropdown from './Dropdown'
+import Alert from 'react-bootstrap/Alert'
 import './Location.css';
+import Button from 'react-bootstrap/Button'
 
 const getLocation = gql`
-  query getLocations($page: Int)  {
-    locations(page: $page) {
+  query getLocations($page: Int, $filter: FilterLocation)  {
+    locations(page: $page, filter: $filter) {
       info {
         pages
         next
@@ -25,52 +29,84 @@ const getLocation = gql`
 
 function Locations() {
   const [activePage, setActivePage] = useState(1)
+  const [filter, setFilter] = useState({
+    name: '',
+    type: '',
+    dimension: ''
+  })
 
-  const { loading, data, error } = useQuery(getLocation, { variables: { page: activePage }} )
-
-  if (loading) return (<Location />)
-  if (error) return `Error: ${error}`
+  const { loading, data, error } = useQuery(getLocation, { variables: { page: activePage, filter }, fetchPolicy: "network-only"  })
 
   const handlePageChange = (pageNumber) => {
-    console.log(`active page is ${pageNumber}`);
     setActivePage(pageNumber);
   }
 
   return (
     <div className="Location container mt-5">
-        <div className="row justify-content-end">
+        {
+          error && <Alert variant="danger">error</Alert>
+        }
+        <div className="row my-4">
           <Pagination
             activePage={activePage}
             itemsCountPerPage={10}
-            totalItemsCount={data.locations.info.count}
+            totalItemsCount={data && data.locations.info.count}
             pageRangeDisplayed={5}
             onChange={handlePageChange}
           />
+          <div className="d-flex justify-content-center button-filters">
+            <Dropdown
+              data={data && data}
+              setFilter={setFilter}
+              title="Name"
+              type="name"
+            />
+            <Dropdown
+              data={data && data}
+              setFilter={setFilter}
+              title="Type"
+              type="type"
+            />
+            <Dropdown
+              data={data && data}
+              setFilter={setFilter}
+              title="Dimension"
+              type="dimension"
+            />
+            <Button onClick={() => setFilter({ name: '', type: '', dimension: '' })} variant="primary">Reset Filters</Button>
+          </div>
         </div>
-      <div className="card-columns justify-content-center">
-        {
-          data.locations.results.map(result => (
-            <div className="card-list">
-              <div className="content">
-                <div className="row">
-                  <div className="col">
-                    <h3>Name: </h3>
-                    <label>{result.name}</label>
-                  </div>
-                  <div className="col">
-                    <h3>Type: </h3>
-                    <label>{result.type}</label>
-                  </div>
-                  <div className="col">
-                    <h3>Dimension: </h3>
-                    <label>{result.dimension}</label>
+      {
+        loading ? (<Skeleton />) : (
+          <div className="card-columns justify-content-center">
+            {
+              data && data.locations.results.map(result => (
+                <div className="card-list">
+                  <div className="content">
+                    <div className="row">
+                        <div className="col">
+                        <h3>Name: </h3>
+                        <label>{result.name}</label>
+                      </div>
+                      <div className="col">
+                        <h3>Type: </h3>
+                        <label>{result.type}</label>
+                      </div>
+                      <div className="col">
+                        <h3>Dimension: </h3>
+                        <label>{result.dimension}</label>
+                      </div>
+                      <div className="col">
+                        <Link to={`/residents/${result.id}`}>Residents</Link>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
-          ))
-        }
-      </div>
+              ))
+            }
+          </div>
+        )
+      }
     </div>
   );
 }
--- src/components/Residents.js
+++ src/components/Residents.js
@@ -26,22 +26,26 @@ export default function Residents() {
   if (error) return `Error: ${error}`
   
   return (
-    <div className="App container">
-      <h1>{data.location.name}</h1>
-      <h4>{data.location.dimension}</h4>
-      {data.location.residents.map(post => (
-        <div className="row justify-content-center mt-5" key={post.id}>
-          <div className="card">
-            <div className="content">
-              <img src={post.image} alt={`Imagen de ${post.name}`}/>
-              <h1 className="m-2">{post.name}</h1>
-              <p>status: {post.status || 'unknown'}</p>
-              <p>specie: {post.species || 'unknown'}</p>
-              <p>type: {post.type || 'unknown'}</p>
+    <React.Fragment>
+      <div className="container-title mt-5">
+        <h1>{data.location.name}</h1>
+        <h4>{data.location.dimension}</h4>
+      </div>
+      <div className="App container mt-5">
+        {data.location.residents.map(post => (
+          <div className="row justify-content-center mt-5" key={post.id}>
+            <div className="card">
+              <div className="content">
+                <img src={post.image} alt={`Imagen de ${post.name}`}/>
+                <h1 className="name-resident m-2">{post.name}</h1>
+                <p>status: {post.status || 'unknown'}</p>
+                <p>specie: {post.species || 'unknown'}</p>
+                <p>type: {post.type || 'unknown'}</p>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </React.Fragment>
   )
 }
